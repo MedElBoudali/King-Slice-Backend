4b50ea55c42f1fb91c9c98a96aaa6a1d6be346de
MedElBoudali <med.sneaky@gmail.com>
2021-04-12 23:52:45 +0000

    🎨 added custom preview

--- schemas/pizza.js
+++ schemas/pizza.js
@@ -23,11 +23,32 @@ export default {
 			}
 		},
 		{
+			name: "toppings",
+			title: "Toppings",
+			type: "array",
+			of: [{ type: "reference", to: [{ type: "topping" }] }]
+		},
+		{
 			name: "price",
 			title: "Price",
 			type: "number",
 			description: "Price of the pizza in cents",
 			validation: Rule => Rule.min(1000).max(50000)
 		}
-	]
+	],
+	preview: {
+		select: {
+			title: "name",
+			media: "image",
+			topping0: "toppings.0.name",
+			topping1: "toppings.1.name",
+			topping2: "toppings.2.name",
+			topping3: "toppings.3.name"
+		},
+		prepare: ({ title, media, ...toppings }) => ({
+			title,
+			media,
+			subtitle: Object.values(toppings).filter(Boolean).join(", ") + "."
+		})
+	}
 };
