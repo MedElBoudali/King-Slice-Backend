2a4208013463b98fe56e6402c64fe7f1d864124b
MedElBoudali <med.sneaky@gmail.com>
2021-04-15 00:09:21 +0000

    🎨 added PriceInput component

--- components/PriceInput.js
+++ components/PriceInput.js
@@ -1,9 +1,21 @@
 import React from "react";
+import PatchEvent, { set, unset } from "part:@sanity/form-builder/patch-event";
 
-const PriceInput = () => {
+const createPatchFrom = value => {
+	PatchEvent.from(value ? set(Number(value)) : unset());
+};
+
+// const formatMoney = Intl.NumberFormat("en-USA", {
+// 	style: "currency",
+// 	currency: "USD"
+// }).format;
+
+const PriceInput = ({ type: { title, description, name }, value, onChange, inputComponent }) => {
 	return (
 		<div>
-			<h2>The Price</h2>
+			{/* <h2>{`${title} ${value ? `- ${formatMoney(value / 100)}` : ""}`}</h2> */}
+			<p>{description}</p>
+			<input type={name} value={value} onChange={e => onChange(createPatchFrom(e.target.value))} ref={inputComponent} />
 		</div>
 	);
 };
--- schemas/pizza.js
+++ schemas/pizza.js
@@ -1,3 +1,6 @@
+import PriceInput from "../components/PriceInput";
+import PizaInput from "../components/PriceInput";
+
 export default {
 	name: "pizza",
 	title: "Pizzas",
@@ -32,6 +35,7 @@ export default {
 			name: "price",
 			title: "Price",
 			type: "number",
+			inputComponent: PriceInput,
 			description: "Price of the pizza in cents",
 			validation: Rule => Rule.min(1000).max(50000)
 		}
