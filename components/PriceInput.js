88eacd279082b14edb6d171ed39cf2884afd38a9
MedElBoudali <med.sneaky@gmail.com>
2021-04-15 14:15:43 +0000

    🎨 bug fixed

--- components/PriceInput.js
+++ components/PriceInput.js
@@ -2,20 +2,22 @@ import React from "react";
 import PatchEvent, { set, unset } from "part:@sanity/form-builder/patch-event";
 
 const createPatchFrom = value => {
-	PatchEvent.from(value ? set(Number(value)) : unset());
+	return PatchEvent.from(value ? set(Number(value)) : unset());
 };
 
-// const formatMoney = Intl.NumberFormat("en-USA", {
-// 	style: "currency",
-// 	currency: "USD"
-// }).format;
+const formatMoney = Intl.NumberFormat("en-ENG", {
+	style: "currency",
+	currency: "USD"
+}).format;
 
 const PriceInput = ({ type: { title, description, name }, value, onChange, inputComponent }) => {
 	return (
 		<div>
-			{/* <h2>{`${title} ${value ? `- ${formatMoney(value / 100)}` : ""}`}</h2> */}
+			<h2>
+				{title} - {formatMoney(value ?? 0)}
+			</h2>
 			<p>{description}</p>
-			<input type={name} value={value} onChange={e => onChange(createPatchFrom(e.target.value))} ref={inputComponent} />
+			<input type={name} value={value ?? 0} onChange={e => onChange(createPatchFrom(e.target.value))} ref={inputComponent} />
 		</div>
 	);
 };
